30c79d8509a5926431fd9c17f4fccf1b8831b008
sunnySavlot <sunny.savlot@gmail.com>
2023-08-29 16:10:32 +0530

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,17 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
-  }
+export let cart = JSON.parse(localStorage.getItem('cart')) || [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1
+    }
 ]
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
 
 export function addToCart(productId) {
     let matchingItem;
@@ -27,15 +31,20 @@ export function addToCart(productId) {
         quantity: Number(selectInput.value), // it addes quantity to the cart array according to the 'select' input's value
       });
     }
+    saveToStorage()
   }
 
 
 export function removeFromCart(productId) {
   let newCart = []
+
   cart.forEach(cartItem => {
     if (cartItem.productId !== productId) {
       newCart.push(cartItem)
     }
   })
+
   cart = newCart
+
+  saveToStorage()
 }
