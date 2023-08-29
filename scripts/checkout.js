8e6f04ba1b78c9af8fd53476d4fc844263163cb0
sunnySavlot <sunny.savlot@gmail.com>
2023-08-29 13:27:09 +0530

    Fixed radio buttons

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -47,38 +47,38 @@ let cartSummaryHTML = ''
 
             <div class="delivery-options">
                 <div class="delivery-options-title">
-                Choose a delivery option:
+                    Choose a delivery option:
                 </div>
                 <div class="delivery-option">
                 <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                 <div>
                     <div class="delivery-option-date">
-                    Tuesday, June 21
+                        Tuesday, June 21
                     </div>
                     <div class="delivery-option-price">
-                    FREE Shipping
+                        FREE Shipping
                     </div>
                 </div>
                 </div>
                 <div class="delivery-option">
                 <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                 <div>
                     <div class="delivery-option-date">
-                    Wednesday, June 15
+                        Wednesday, June 15
                     </div>
                     <div class="delivery-option-price">
-                    $4.99 - Shipping
+                        $4.99 - Shipping
                     </div>
                 </div>
                 </div>
                 <div class="delivery-option">
                 <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                 <div>
                     <div class="delivery-option-date">
                     Monday, June 13
